773999c1058785868bf8a52aa31f45b43991157f
Sheldon Hart <sheldon.hart88@gmail.com>
2025-04-14 15:01:40 +0200

    Add initial project setup with redirect functionality and configuration files

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request): Promise<Response> {
+		const base = 'https://www.fromthehart.tech';
+		const statusCode = 301;
+
+		const url = new URL(request.url);
+		const { pathname, search } = url;
+
+		const destinationURL = `${base}${pathname}${search}`;
+		console.log(destinationURL);
+
+		return Response.redirect(destinationURL, statusCode);
 	},
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler;
